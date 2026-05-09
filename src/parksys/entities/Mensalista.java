07f965cd3f88c68bc4b5639b759b035a69e24c0f
agbram <agbram2005@gmail.com>
2026-05-09 08:33:38 -0300

    feat(S01): criar entidade Mensalista

---
+++ src/parksys/entities/Mensalista.java
@@ -0,0 +1,48 @@
+package parksys.entities;
+
+import parksys.enums.TipoVeiculo;
+
+import java.time.LocalDate;
+
+public class Mensalista extends Veiculo {
+    private String nome;
+    private double mensalidade;
+    private LocalDate validade;
+
+
+    public Mensalista(String placa, TipoVeiculo tipoVeiculo, String nome, double mensalidade, LocalDate validade) {
+        super(placa, tipoVeiculo);
+        this.nome = nome;
+        this.mensalidade = mensalidade;
+        this.validade = validade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getMensalidade() {
+        return mensalidade;
+    }
+
+    public void setMensalidade(double mensalidade) {
+        this.mensalidade = mensalidade;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - " + getPlaca() + " - Valor: " + mensalidade + "\n" + "Válido até: " + validade;
+    }
+}
