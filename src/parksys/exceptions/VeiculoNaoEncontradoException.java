4e2067ee8daa110cfc17b6da27a2ad4162f97f93
agbram <agbram2005@gmail.com>
2026-05-09 08:40:41 -0300

    feat(S01): criar exception VeiculoNaoEncontradoException

---
+++ src/parksys/exceptions/VeiculoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package parksys.exceptions;
+
+public class VeiculoNaoEncontradoException extends RuntimeException {
+    public VeiculoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
